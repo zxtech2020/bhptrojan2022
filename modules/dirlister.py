bcbdcfa0834e063b61afd4ed98c6081ed3811184
kwkl <kwkl@kwkl.kwkl>
2022-10-16 17:17:47 +0800

    add dirlister environment module

---
+++ modules/dirlister.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+# Hi There!
+#aronlab
+# need debug
+
+#utf-8
+
+import os
+
+def run(**args):
+    print("[*] In dirlister module.")
+    files=os.listdir(".")
+    return str(files)
+
+    
+    
